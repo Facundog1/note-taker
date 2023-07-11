721e1c439f3a0ea81f07f323e2095ef3209e8ca8
Facundo Dawson <f.dawson66@gmail.com>
2023-07-11 18:49:11 -0500

    added uuid and refactored server'

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const path = require('path');
 const PORT = process.env.PORT ?? 3001;
 const app = express();
+const fs = require('fs');
+const routes = require('./routes');
 
 app.use(express.static('public'));
 
@@ -13,6 +15,13 @@ app.get ('*', (req, res) => {
     return res.sendFile(path.join(__dirname, 'public/index.html'));
   });
 
+  app.post ('/api/notes', (req, res) => {
+
+  }
+
+app.get('/api/notes', (req, res) => {
+  return res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Application is running @ http://localhost:${PORT}`);
